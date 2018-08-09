51382626e9a53493ea4279702122d7f297641b39
desaigner <sai@desaign.me>
2018-08-09 15:13:56 -0500

    18324C02AB

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,7 +17,7 @@
 
 3.  computerChoices
         - give computer some choices of Words ("actor", "agent", "alone", "anger", "avoid", "beach", "birth", "brown", "built", "buyer", "chair", "china", "clean", "court", "crown")l
-            - make sure the word that computer select clue also connect with that word...
+            - make sure the word that computer select clue also connect with that word... (under construction!)
 
         - letterToGuess
             - let computer guess random word from choices
@@ -37,24 +37,7 @@
 
         - Let user select one key
             - onKeyUp
-                - userGuess
-                    - if userGuess match letterToGuess
-                        - wins ++
-                        - reset()
-                        - update note to say "you are psychic"
-                    - if userGuess is not matching
-                        - check if guessLeft > 0
-                            - if
-                                - check userGuess is not repeating
-                                    - if 
-                                        - return false
-                                    - else
-                                        - put this guess into guessedLetters
-                                        - update note to say "keep trying"
-                            - else
-                                - loose ++
-                                - reset()
-                                - update note to say "you are not psychic, try again!"
+
 
 
 
@@ -66,10 +49,6 @@
         - updateGuessesSoFar();
         - updateComputerGuess(); // background color change
 
-6.  Extra
-        - colors
-            - change background color everytime when update on computer guess //updateComputerGuess()
-
 */
 
 //1
@@ -80,49 +59,60 @@ var losses = 0;
 var guesses = 9;
 var guessesLeft = 9;
 var guessedLetters = [];
+var wordToGuess = null;
 var letterToGuess1 = null;
 var letterToGuess2 = null;
 var letterToGuess3 = null;
 var letterToGuess4 = null;
 var letterToGuess5 = null;
-
-//6
-var color = "rgb(" + Math.floor(Math.random() * 56) + "," + Math.floor(Math.random() * 56) + "," + Math.floor(Math.random() * 56) + ")"
-
-var updateComputerGuess = function() {
-    document.querySelector('.bg').style.backgroundColor = color;
-};
+var guess1 = false;
+var guess2 = false;
+var guess3 = false;
+var guess4 = false;
+var guess5 = false;
 
 //3
 var computerChoices = ["actor", "agent", "alone", "anger", "avoid", "beach", "birth", "brown", "built", "buyer", "chair", "china", "clean", "court", "crown"];
 
-var wordToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-console.log(wordToGuess);
-
-//computer guess one letter and once it match (or not) it updates
-var updateLetterToGuess = function() {
+var updateWordToGuess = function() {
+    this.wordToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
     this.letterToGuess1 = this.wordToGuess.charAt(0);
     this.letterToGuess2 = this.wordToGuess.charAt(1);
     this.letterToGuess3 = this.wordToGuess.charAt(2);
     this.letterToGuess4 = this.wordToGuess.charAt(3);
     this.letterToGuess5 = this.wordToGuess.charAt(4);
-};
-
+}
 
 //5
 var reset = function() {
     guessesLeft = 9;
     guessedLetters = [];
-    updateLetterToGuess();
+    letterToGuess1 = null;
+    letterToGuess2 = null;
+    letterToGuess3 = null;
+    letterToGuess4 = null;
+    letterToGuess5 = null;
+    guess1 = false;
+    guess2 = false;
+    guess3 = false;
+    guess4 = false;
+    guess5 = false;
+
+    document.querySelector('#compGuessDisplay1').innerHTML = "?";
+    document.querySelector('#compGuessDisplay2').innerHTML = "?";
+    document.querySelector('#compGuessDisplay3').innerHTML = "?";
+    document.querySelector('#compGuessDisplay4').innerHTML = "?";
+    document.querySelector('#compGuessDisplay5').innerHTML = "?";
+
+    updateWordToGuess();
     updateGuessesLeft();
     updateGuessesSoFar();
-    updateComputerGuess();
+
+    document.querySelector('#youGuessLetters').innerHTML = "?";
 };
 
 //4
 var updateGuessesLeft = function() {
-    // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
     if (guessesLeft >= 2) {
         document.querySelector("#aNumChg").innerHTML = "You have <span style='white-space: nowrap;'>" + guessesLeft + " chances</span> <span style='white-space: nowrap;'>to match that</span> character!";
     } else { document.querySelector("#aNumChg").innerHTML = "You have last chance <span style='white-space: nowrap;'>to match that</span> character!"; }
@@ -132,14 +122,12 @@ var updateGuessesSoFar = function() {
     document.querySelector('#youGuessLetters').innerHTML = guessedLetters.join(', ');
 };
 
-updateLetterToGuess();
+updateWordToGuess();
 updateGuessesLeft();
 
 document.onkeyup = function(event) {
 
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-    console.log(userGuess);
 
     function check(obj, list) {
         var i;
@@ -149,54 +137,48 @@ document.onkeyup = function(event) {
             }
         }
         list.push(obj);
-        guessesLeft--;
     }
 
     check(userGuess, guessedLetters);
 
     updateGuessesLeft();
     updateGuessesSoFar();
-    console.log(letterToGuess1);
+
+    if (userGuess === letterToGuess1) {
+        document.querySelector('#compGuessDisplay1').innerHTML = letterToGuess1;
+        guess1 = true;
+    } else if (userGuess === letterToGuess2) {
+        document.querySelector('#compGuessDisplay2').innerHTML = letterToGuess2;
+        guess2 = true;
+    } else if (userGuess === letterToGuess3) {
+        document.querySelector('#compGuessDisplay3').innerHTML = letterToGuess3;
+        guess3 = true;
+    } else if (userGuess === letterToGuess4) {
+        document.querySelector('#compGuessDisplay4').innerHTML = letterToGuess4;
+        guess4 = true;
+    } else if (userGuess === letterToGuess5) {
+        document.querySelector('#compGuessDisplay5').innerHTML = letterToGuess5;
+        guess5 = true;
+    } else {
+        guessesLeft--;
+    }
 
     if (guessesLeft > 0) {
-        for (var j = 0; j < 5; j++) {
-            if (userGuess == letterToGuess[j]) {
-                wins++;
-                document.querySelector('#win').innerHTML = "Wins: " + wins;
-                document.querySelector('#areYouReady').innerHTML = "Yes, you are psychic!!";
-                document.querySelector('#compGuessDisplay1').innerHTML = letterToGuess1;
-                document.querySelector('#compGuessDisplay2').innerHTML = letterToGuess2;
-                document.querySelector('#compGuessDisplay3').innerHTML = letterToGuess3;
-                document.querySelector('#compGuessDisplay4').innerHTML = letterToGuess4;
-                document.querySelector('#compGuessDisplay5').innerHTML = letterToGuess5;
-                reset();
-            } else {
-                document.querySelector('#areYouReady').innerHTML = "Keep trying!";
-            }
+
+        if (guess1 && guess2 && guess3 && guess4 && guess5) {
+            wins++;
+            document.querySelector('#win').innerHTML = "&#128077; : " + wins;
+            document.querySelector('#areYouReady').innerHTML = "Yes, you are psychic!!";
+            reset();
+            console.log(reset);
+        } else {
+            document.querySelector('#areYouReady').innerHTML = "Keep trying!";
         }
-        // if (userGuess == letterToGuess1) {
-        //     wins++;
-        //     document.querySelector('#win').innerHTML = "Wins: " + wins;
-        //     document.querySelector('#areYouReady').innerHTML = "Yes, you are psychic!!";
-        //     document.querySelector('#compGuessDisplay1').innerHTML = letterToGuess1;
-        //     document.querySelector('#compGuessDisplay2').innerHTML = letterToGuess2;
-        //     document.querySelector('#compGuessDisplay3').innerHTML = letterToGuess3;
-        //     document.querySelector('#compGuessDisplay4').innerHTML = letterToGuess4;
-        //     document.querySelector('#compGuessDisplay5').innerHTML = letterToGuess5;
-        //     reset();
-        // } else {
-        //     document.querySelector('#areYouReady').innerHTML = "Keep trying!";
-        // }
 
     } else if (guessesLeft == 0) {
         losses++;
-        document.querySelector('#loss').innerHTML = "Losses: " + losses;
-        document.querySelector('#areYouReady').innerHTML = "Nope, you are NOT psychic, play again!";
-        document.querySelector('#compGuessDisplay1').innerHTML = letterToGuess1;
-        document.querySelector('#compGuessDisplay2').innerHTML = letterToGuess2;
-        document.querySelector('#compGuessDisplay3').innerHTML = letterToGuess3;
-        document.querySelector('#compGuessDisplay4').innerHTML = letterToGuess4;
-        document.querySelector('#compGuessDisplay5').innerHTML = letterToGuess5;
+        document.querySelector('#loss').innerHTML = "&#128078; : " + losses;
+        document.querySelector('#areYouReady').innerHTML = "Nope, right word was <span class='text-danger'>" + wordToGuess + "</span>, play again!";
         reset();
     }
 };
